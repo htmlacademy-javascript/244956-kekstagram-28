6e77e05ec96622db67f772c81d8df2fbe0f17b3a
Keks <keks@htmlacademy.ru>
2023-03-24 09:16:04 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,10 @@
+
+import './random-functions.js';
+import './pictures.js';
+import {openUserModal, closeUserModal} from './modal-bigphoto.js';
+import './big-photo.js';
+import './load-picture.js';
+
+
+openUserModal();
+closeUserModal();
